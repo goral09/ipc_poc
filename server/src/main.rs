2073cdd6816676e2c22f14f41d9d49d01345b4b4
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-07 11:57:00 +0100

    Implement request-response pattern.

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,11 +4,9 @@ extern crate protobuf;
 extern crate serde;
 
 use commons::models::Person;
-
 use clap::{App, Arg};
+use std::os::unix::net::{UnixListener};
 use protobuf::Message;
-use std::io::prelude::*;
-use std::os::unix::net::UnixListener;
 
 fn main() {
     let matches = App::new("rust_server")
@@ -16,7 +14,8 @@ fn main() {
             Arg::with_name("socket")
                 .required(true)
                 .help("Socket address"),
-        ).arg(Arg::with_name("e").short("e").help("Echo server"))
+        )
+        .arg(Arg::with_name("e").short("e").help("Echo server"))
         .get_matches();
 
     let socket = matches.value_of("socket").unwrap();
@@ -30,21 +29,17 @@ fn main() {
         std::fs::remove_file(socket_path).unwrap();
     }
 
-    let listener = match UnixListener::bind(socket) {
-        Err(err) => panic!("Failed to bind to socket: {}.", err),
-        Ok(stream) => stream,
-    };
+    let unix_listener = UnixListener::bind(&socket_path).unwrap();
 
     println!("Listening on `{}`. Is echo? {}.", socket, echo);
 
-    for mut stream in listener.incoming() {
+    for mut stream in unix_listener.incoming() {
         match stream {
             Ok(ref mut stream) => {
                 println!("New connection.");
                 let msg = protobuf::parse_from_reader::<Person>(stream).unwrap();
                 println!("Client said: {:?}", msg);
-                //msg.write_to_writer(stream).unwrap();
-                //println!("Sending echo back");
+                msg.write_to_writer(stream).expect("Couldn't write back to client.");
             }
             Err(err) => panic!("Error occured when listening from the stream. {}", err),
         }
